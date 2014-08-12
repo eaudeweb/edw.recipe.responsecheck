96bdd2af35bc534942351fc48c27e8e3a85ec84f
Vitalie Maldur <maldur.vitalik@gmail.com>
2014-08-12 15:38:31 +0300

    PEP8 formatting

--- edw/recipe/responsecheck/__init__.py
+++ edw/recipe/responsecheck/__init__.py
@@ -1,45 +1,60 @@
 # -*- coding: utf-8 -*-
 """Recipe responsecheck"""
-import logging, os, zc.buildout
+import logging
+
+import os
+
+import zc.buildout
+
 
 class Recipe(object):
+    """Recipe class"""
 
     def __init__(self, buildout, name, options):
         self.buildout, self.name, self.options = buildout, name, options
         self.logger = logging.getLogger(self.name)
 
         if 'start-port' not in options or not options['start-port'].isdigit():
-            self.logger.error('Parameter start-port not specified or not a number.')
-            raise zc.buildout.UserError('Parameter start-port not specified or not a number.')
+            self.logger.error(
+                'Parameter start-port not specified or not a number.')
+            raise zc.buildout.UserError(
+                'Parameter start-port not specified or not a number.')
 
         if 'end-port' not in options or not options['end-port'].isdigit():
-            self.logger.error('Parameter end-port not specified or not a number.')
-            raise zc.buildout.UserError('Parameter end-port not specified or not a number.')
+            self.logger.error(
+                'Parameter end-port not specified or not a number.')
+            raise zc.buildout.UserError(
+                'Parameter end-port not specified or not a number.')
 
         if 'step' not in options or not options['step'].isdigit():
             self.logger.error('Parameter step not specified or not a number.')
-            raise zc.buildout.UserError('Parameter step not specified or not a number.')
+            raise zc.buildout.UserError(
+                'Parameter step not specified or not a number.')
 
         if 'backends' not in options:
             self.logger.error('Parameter backends not specified.')
             raise zc.buildout.UserError('Parameter backends not specified.')
 
         if 'path' not in options or not os.path.isdir(options['path']):
-            self.logger.error('Parameter path not specified or directory does not exists.')
-            raise zc.buildout.UserError('Parameter path not specified or directory does not exists.')
+            self.logger.error(
+                'Parameter path not specified or directory does not exists.')
+            raise zc.buildout.UserError(
+                'Parameter path not specified or directory does not exists.')
 
         self.start_port = options['start-port']
         self.end_port = options['end-port']
-        self.step  = options['step']
+        self.step = options['step']
         self.backends = options['backends'].split(' ')
         self.path = options['path'] + '/responsecheck.sh'
 
     def write_script(self):
+        """Method for writing a bash script in a specified file"""
         script = open(self.path, 'w')
 
         script.write('#!/bin/bash\n')
         script.write('echo "*** Check instances have no errors ***";\n')
-        script.write('for i in `seq %s %s %s`;\n' % (self.start_port, self.step, self.end_port))
+        script.write('for i in `seq %s %s %s`;\n' % (
+        self.start_port, self.step, self.end_port))
         script.write('do\n')
 
         script.write('\techo "Checking instance $i"\n')
@@ -50,7 +65,8 @@ class Recipe(object):
 
         script.write('\tfor backend in %s;\n' % backends_list)
         script.write('\tdo\n')
-        curl = 'curl --write-out %{http_code} --connect-timeout 5 --silent --output /dev/null http://$backend:$i/www'
+        curl = 'curl --write-out %{http_code} --connect-timeout 5 --silent' +\
+               ' --output /dev/null http://$backend:$i/www'
         script.write('\t\tresponse=$(%s)\n' % curl)
         script.write('\t\tresult="SUCCESS"\n')
         script.write('\t\tif [ $response -ne 200 ]\n')
@@ -65,9 +81,11 @@ class Recipe(object):
         os.chmod(self.path, 0755)
 
     def install(self):
+        """Method that install this recipe"""
         self.write_script()
         self.options.created(self.path)
         return self.options.created()
 
     def update(self):
+        """Method that update this recipe"""
         self.write_script()
